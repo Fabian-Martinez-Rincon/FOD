7b7681523a8be13863878149166d72f298a91ba9
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-12-30 15:03:51 -0300

    Ejercicio 1

---
+++ Practicas/Practica 1/1.pas
@@ -0,0 +1,22 @@
+program untitled;
+
+uses crt;
+type 
+
+archivoNumeros= file of integer;
+
+var fileName:string;
+archivo:archivoNumeros;
+numero:integer;
+BEGIN
+	clrscr;
+	write('Ingrese nombre de archivo:' ); readln(fileName);
+	assign(archivo,fileName);
+	rewrite (archivo); // se crea el archivo
+	write (' Ingrese un numero : ');readln(numero);
+	while numero <> 0 do begin
+		write(archivo,numero);
+		write (' Ingrese un numero : ');readln(numero);
+	end;
+	close (archivo);
+END.
