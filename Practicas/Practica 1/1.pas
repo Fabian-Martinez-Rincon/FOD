6ee9a039d02320b76b98550a7dcdf3b2e2364e3b
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-12-30 18:55:39 -0300

    documentos

--- Practicas/Practica 1/1.pas
+++ Practicas/Practica 1/1.pas
@@ -10,13 +10,16 @@ archivo:archivoNumeros;
 numero:integer;
 BEGIN
 	clrscr;
-	write('Ingrese nombre de archivo:' ); readln(fileName);
+	write('Ingrese nombre de archivo:' ); 
+	readln(fileName);
 	assign(archivo,fileName);
 	rewrite (archivo); // se crea el archivo
-	write (' Ingrese un numero : ');readln(numero);
+	write (' Ingrese un numero : ');
+	readln(numero);
 	while numero <> 0 do begin
 		write(archivo,numero);
-		write (' Ingrese un numero : ');readln(numero);
+		write (' Ingrese un numero : ');
+		readln(numero);
 	end;
 	close (archivo);
 END.
---
+++ Practicas/Practica 1/Pruebas.pas
@@ -0,0 +1,20 @@
+Program Generar_Archivo;
+type 
+    archivo = file of integer;  {definición del tipo de dato para el archivo }
+var 
+    arc_logico: archivo;        {variable que define el nombre lógico del archivo}
+    nro: integer;               {nro será utilizada para obtener la información de teclado}
+    arc_fisico: string[12];     {utilizada para obtener el nombre físico del archivo desde teclado}
+begin
+    write( 'Ingrese el nombre del archivo:' );
+    read( arc_fisico );         { se obtiene el nombre del archivo}
+    assign( arc_logico, arc_fisico );
+    rewrite( arc_logico );      { se crea el archivo }
+    read( nro );                { se obtiene de teclado el primer valor }
+    while nro <> 0 do 
+    begin
+        write( arc_logico, nro ); { se escribe en el archivo cada número }
+        read( nro );
+    end;
+    close( arc_logico );          { se cierra el archivo abierto oportunamente con la instrucción rewrite }
+end. 
