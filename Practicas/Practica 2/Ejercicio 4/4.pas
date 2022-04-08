f58debcb65eed9898d73a95318f680516ea74372
Fabo xw <fabianmartinezrincon.123@gmail.com>
2022-04-08 10:47:53 -0300

    ejercicios

--- Practicas/Practica 2/Ejercicio 4/4.pas
+++ Practicas/Practica 2/Ejercicio 4/4.pas
@@ -1,4 +1,100 @@
 program cuatro;
+const
+    valorAlto = 9999;
+    dimf = 5;         //Cantidad de maquinas
+type
+    cadena20 = string[20];
+    regDetalle = record
+        cod_usuario:integer;
+        fecha:integer;
+        tiempo_sesion:integer;
+    end;
+    regMaestro = record
+        cod_usuario:integer;
+        fecha:integer;
+        tiempo_total_de_sesiones_abiertas:integer;
+    end;
+
+    archMaestro = file of regMaestro;
+    archDetalle = file of regDetalle;
+
+    vector_detalle_registro = array [1..dimf] of regDetalle;
+    vector_detalle = array [1..dimf] of archDetalle;
+//___________________________________________________
+procedure Leer(var detalle:archDetalle; var datoD:regDetalle);
 begin
-  
+    if(not EOF(detalle)) then
+        read(detalle,datoD)
+    else
+        datoD.cod_usuario:=valorAlto;
+end;
+//___________________________________________________
+procedure minimo(var vDetalle:vector_detalle;var vDetalleR:vector_detalle_registro;var min:regDetalle);
+var
+    i,minPos:integer;
+begin
+    min.cod_usuario:=9999;
+    min.fecha:=9999;
+    for i:=1 to dimf do 
+    begin
+        if ((vDetalleR[i]).cod_usuario < min.cod_usuario) then
+        begin
+            if((vDetalleR[i]).fecha < min.fecha)then
+            begin
+                min:=vDetalleR[i];
+                minPos:=i;
+            end;
+        end;
+    end;
+    if(min.cod_usuario <> 9999)then
+        Leer(vDetalle[minPos],vDetalleR[minPos]);
+end;
+//___________________________________________________
+procedure crearMestro(var vDetalle:vector_detalle;var vDetalleR:vector_detalle_registro;var maestro:archMaestro);
+var
+    min:regDetalle;
+    actual:regMaestro;
+    i:integer;
+begin
+    Assign(maestro, 'maestro.data');
+    Rewrite(maestro);
+    minimo(vDetalle,vDetalleR,min);
+    while min.cod_usuario <> 9999 do
+    begin
+        actual.cod_usuario:=min.cod_usuario;
+        while actual.cod_usuario = min.cod_usuario do
+        begin
+            actual.fecha:=min.fecha;
+            actual.tiempo_total_de_sesiones_abiertas:=0;
+            while (min.cod_usuario <> 9999) and (min.cod_usuario = actual.cod_usuario) and (min.fecha = actual.fecha) do
+            begin
+                actual.tiempo_total_de_sesiones_abiertas:= actual.tiempo_total_de_sesiones_abiertas + min.tiempo_sesion;
+                minimo(vDetalle,vDetalleR,min);
+            end;
+            WriteLn(actual.cod_usuario, ' ', actual.fecha, ' ', actual.tiempo_total_de_sesiones_abiertas);
+            Write(maestro,actual);
+        end;
+    end;
+    Close(maestro);
+
+end;
+//___________________________________________________
+var
+    i:integer;
+    iString:cadena20;
+    vDetalleR:vector_detalle_registro;
+    vDetalle:vector_detalle;
+    maestro:archMaestro;
+begin
+    iString:='';
+    for i := 1 to dimf do
+    begin
+        
+        Str(i,iString);
+        Assign(vDetalle[i], 'detalle'+iString);
+        Rewrite(vDetalle[i]);
+        Leer(vDetalle[i],vDetalleR[i]);
+        Close(vDetalle[i]); 
+    end;
+    crearMestro(vDetalle,vDetalleR,maestro);
 end.
---
+++ Practicas/Practica 2/Ejercicio 5/5.pas
@@ -0,0 +1,190 @@
+program cinco;
+const
+    dimF = 5; //Tendira que ser 50
+    valorAlto = 9999;
+type
+    cadena20 = string[20];
+    direccion = record
+        calle:integer;
+        nro:integer;
+        piso:integer;
+        depto:integer;
+        ciudad:cadena20;
+    end;
+    datos = record
+        nombre:cadena20;
+        apellido:cadena20;
+        dni:cadena20;
+    end;
+    nacimiento = record
+        nroNaci:integer;
+        nombre:cadena20;
+        apellido:cadena20;
+        dir:direccion;
+        matricula:integer;
+        madre:datos;
+        padre:datos;
+    end;
+    
+    fallecimiento = record
+        nroNaci:integer;
+        dni:integer;
+        nombre:cadena20;
+        apellido:cadena20;
+        matricula:integer; //Matricula del medico
+        fecha:cadena20;
+        hora:integer;
+        lugar:cadena20;
+    end;
+    
+    RegFallecio = record
+        matricula:integer;
+        fecha:integer;
+        hora:integer;
+        lugar:cadena20;
+    end;
+    regMaestro = record
+        nroNaci:integer;
+        nombre:cadena20;
+        apellido:cadena20;
+        dir:direccion;
+        matricula:Integer;
+        madre:datos;
+        padre:datos;
+        siFallecio:RegFallecio;
+    end;
+
+    archNacimiento = file of nacimiento;
+    nacimientos = array [1..dimF] of archNacimiento;
+    nacimientoReg = array [1..dimF] of nacimiento;
+
+    archFallecimiento = file of fallecimiento;
+    fallecimientos = array [1..dimF] of archFallecimiento;
+    fallecimientosReg = array [1..dimF] of fallecimiento; 
+
+    archMaestro = file of regMaestro;
+//________________________________________________________________________
+procedure LeerN(var detalle:archNacimiento; var datoD:nacimiento);
+begin
+    if(not EOF(detalle)) then
+        read(detalle,datoD)
+    else
+        datoD.nroNaci:=valorAlto;
+end;
+//________________________________________________________________________
+procedure LeerF(var detalle:archFallecimiento; var datoD:fallecimiento);
+begin
+    if(not EOF(detalle)) then
+        read(detalle,datoD)
+    else
+        datoD.nroNaci:=valorAlto;
+end;
+//________________________________________________________________________
+procedure minimoN(var vN:nacimientos;var vNR:nacimientoReg;var minN:nacimiento);
+var
+    i,minPos:integer;
+begin
+    minN.nroNaci:=9999;
+    for i:=1 to dimF do begin
+        if (vNR[i].nroNaci < minN.nroNaci) then begin
+            minN:=vNR[i];
+            minPos:=i;
+        end;
+    end;
+    if (minN.nroNaci <> valorAlto) then
+        LeerN(vN[minPos],vNR[minPos]);
+end;
+//________________________________________________________________________
+procedure minimoF(var vF:fallecimientos;var vNR:fallecimientosReg;var minF:fallecimiento);
+var
+    i,minPos:integer;
+begin
+    minF.nroNaci:=9999;
+    for i:=1 to dimF do begin
+        if (vNR[i].nroNaci < minF.nroNaci) then begin
+            minF:=vNR[i];
+            minPos:=i;
+        end;
+    end;
+    if (minF.nroNaci <> valorAlto) then
+        LeerF(vF[minPos],vNR[minPos]);
+end;
+//________________________________________________________________________
+procedure cargarMaestroN(var actual:regMaestro;minN:nacimiento);
+begin
+    actual.nroNaci:=minN.nroNaci;
+    actual.nombre:=minN.nombre;
+    actual.apellido:=minN.apellido;
+    actual.dir:=minN.dir;
+    actual.matricula:=minN.matricula;
+    actual.madre:=minN.madre;
+    actual.padre:=minN.padre;
+    //Como es de nacimiento supongo que los pongo en blanco de momento :(
+    actual.siFallecio.matricula:=-1;
+    actual.siFallecio.fecha:=-1;
+    actual.siFallecio.hora:=-1;
+    actual.siFallecio.lugar:=' ';
+
+end;
+//________________________________________________________________________
+{procedure cargarMaestroF(var actual:regMaestro;minF:fallecimiento);
+begin
+    actual.siFallecio.matricula:=minF.
+end;}
+//________________________________________________________________________
+procedure crearMaestro(var maestro:archMaestro;var vN:nacimientos;var vNR:nacimientoReg;var vF:fallecimientos;var vFR:fallecimientosReg);
+var
+    actual:regMaestro;
+    i:integer;
+    minN:nacimiento; 
+    minF:fallecimiento;
+begin
+    Assign(maestro, 'siniestro.data');
+    Rewrite(maestro);
+    minimoN(vN,vNR,minN);
+    while (minN.nroNaci <> valorAlto) do
+    begin
+        cargarMaestroN(actual,minN);
+        write(maestro,actual);
+        minimoN(vN,vNR,minN);
+    end;
+    //Esto lo hago para volver al primer elemento y agregar si fallecieron
+    Close(maestro); 
+    Reset(maestro);
+    minimoF(vF,vFR,minF);
+    while (minF.nroNaci <> valorAlto) and (not Eof(maestro)) do
+    begin
+        while (actual.nroNaci <> minF.nroNaci) do
+            Read(maestro,actual);
+        actual.siFallecio.matricula:=minF.matricula;
+        actual.siFallecio.fecha:=minF.fecha;
+        actual.siFallecio.hora:=minF.hora;
+        actual.siFallecio.lugar:=minF.lugar;
+        seek(maestro,FilePos(maestro)-1);
+        
+        //cargarMaestroF(actual,minF);
+    end;
+    
+
+
+end;
+//________________________________________________________________________
+var
+    i:integer;
+    iString:cadena20;
+    maestro:archMaestro;
+    vN:nacimientos;     vNR:nacimientoReg;
+    vF:fallecimientos;  vFR:fallecimientosReg;
+begin
+    for i:=1 to dimF do
+    begin
+        Str(i,iString);
+        Assign(vN[i],'detalleN'+iString); Assign(vF[i],'detalleF'+iString);
+        Reset(vN[i]);                     Reset(vF[i]);
+        
+        LeerN(vN[i],vNR[i]);              LeerF(vF[i],vFR[i]);
+
+        Close(vN[i]);                     Close(vF[i]); //Esto hacerlo fuera porque voy a seguir trabajando con el archivo
+    end;
+    crearMaestro(maestro,vN,vNR,vF,vFR);
+end.
